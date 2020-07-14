84248b5df96cd792aeb957ca2751cb6310c2fee2
Andres Toledo <49215828+AndresMToledo@users.noreply.github.com>
2020-07-14 13:09:05 -0400

    Created Project

---
+++ ProjectService/src/test/java/com/anjowe/behive/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjowe.behive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
